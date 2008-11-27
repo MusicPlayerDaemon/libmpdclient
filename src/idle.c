8a9a0d7aa7cdd13d5f3b0f66439504557c8a90b4
Marc Pavot <marc.pavot@gmail.com>
2008-11-27 21:11:30 +0100

    Set "local" includes first for idle.c
    This is done to test if the include file has specified its own dependencies properly.

--- src/idle.c
+++ src/idle.c
@@ -30,9 +30,9 @@
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include "idle.h"
 #include <stdio.h>
 #include <string.h>
-#include "idle.h"
 
 static const char *const idle_names[] = {
 	"database",
