MusicPlayerDaemon/libmpdclient

commit d3c8bd9b5479b8dcb05022c81940878645364837

idle: added function mpd_idle_parse_pair()

src/idle.c
@@ -52,6 +52,21 @@ static const char *const idle_names[] = {
 };
 
 enum mpd_idle
+mpd_idle_parse_pair(const struct mpd_pair *pair)
+{
+	assert(pair != NULL);
+
+	if (strcmp(pair->name, "changed") != 0)
+		return 0;
+
+	for (unsigned i = 0; idle_names[i] != NULL; ++i)
+		if (strcmp(pair->value, idle_names[i]) == 0)
+			return 1 << i;
+
+	return 0;
+}
+
+enum mpd_idle
 mpd_recv_idle(struct mpd_connection *connection)
 {
 	enum mpd_idle flags = 0;
@@ -59,13 +74,8 @@ mpd_recv_idle(struct mpd_connection *connection)
 
 	assert(connection != NULL);
 
-	while ((pair = mpd_recv_pair_named(connection, "changed")) != NULL) {
-		for (unsigned i = 0; idle_names[i] != NULL; ++i)
-			if (strcmp(pair->value, idle_names[i]) == 0)
-				flags |= (1 << i);
-
-		mpd_return_pair(connection, pair);
-	}
+	while ((pair = mpd_recv_pair(connection)) != NULL)
+		flags |= mpd_idle_parse_pair(pair);
 
 	return flags;
 }

include/mpd/idle.h
@@ -35,6 +35,7 @@
 
 #include <stdbool.h>
 
+struct mpd_pair;
 struct mpd_connection;
 
 /**
@@ -87,6 +88,15 @@ mpd_send_idle(struct mpd_connection *connection);
  */
 bool
 mpd_send_noidle(struct mpd_connection *connection);
+
+/**
+ * Parses a "changed" pair, which is part of MPD's response to the
+ * "idle" command.
+ *
+ * @return an idle code, or 0 if the pair was not understood
+ */
+enum mpd_idle
+mpd_idle_parse_pair(const struct mpd_pair *pair);
 
 /**
  * Waits until MPD sends the list of idle events and returns it in a

commit bdcb27df789414d71775247281d26ae857a75eb0

idle: return pairs in mpd_recv_idle()
Broken by d3c8bd9b.

src/idle.c
@@ -109,8 +109,11 @@ mpd_recv_idle(struct mpd_connection *connection)
 		.tv_usec = 0,
 	};
 
-	while ((pair = mpd_recv_pair(connection)) != NULL)
+	while ((pair = mpd_recv_pair(connection)) != NULL) {
 		flags |= mpd_idle_parse_pair(pair);
+
+		mpd_return_pair(connection, pair);
+	}
 
 	/* reenable timeout */
 	connection->timeout = old_timeout;