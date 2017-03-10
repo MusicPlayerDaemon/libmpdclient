MusicPlayerDaemon/libmpdclient

commit 11b76ed4527d17153fc12f289bc1d59fabb6ea30

Add support for ArtistSort and AlbumArtistSort tags

include/mpd/tag.h
@@ -42,8 +42,10 @@ enum mpd_tag_type
 	MPD_TAG_UNKNOWN = -1,
 
 	MPD_TAG_ARTIST,
+	MPD_TAG_ARTIST_SORT,
 	MPD_TAG_ALBUM,
 	MPD_TAG_ALBUM_ARTIST,
+	MPD_TAG_ALBUM_ARTIST_SORT,
 	MPD_TAG_TITLE,
 	MPD_TAG_TRACK,
 	MPD_TAG_NAME,

src/tag.c
@@ -39,8 +39,10 @@
 static const char *const mpd_tag_type_names[MPD_TAG_COUNT] =
 {
 	"Artist",
+	"ArtistSort",
 	"Album",
 	"AlbumArtist",
+	"AlbumArtistSort",
 	"Title",
 	"Track",
 	"Name",

commit 42830bcd8bc1054fb7933ba3cafaa5a2bd31284e

tag: restore original enum integer values
Fixes major ABI breakage by commit 11b76ed452

include/mpd/tag.h
@@ -46,10 +46,8 @@ enum mpd_tag_type
 	MPD_TAG_UNKNOWN = -1,
 
 	MPD_TAG_ARTIST,
-	MPD_TAG_ARTIST_SORT,
 	MPD_TAG_ALBUM,
 	MPD_TAG_ALBUM_ARTIST,
-	MPD_TAG_ALBUM_ARTIST_SORT,
 	MPD_TAG_TITLE,
 	MPD_TAG_TRACK,
 	MPD_TAG_NAME,
@@ -65,6 +63,9 @@ enum mpd_tag_type
 	MPD_TAG_MUSICBRAINZ_ALBUMARTISTID,
 	MPD_TAG_MUSICBRAINZ_TRACKID,
 	MPD_TAG_MUSICBRAINZ_RELEASETRACKID,
+
+	MPD_TAG_ARTIST_SORT,
+	MPD_TAG_ALBUM_ARTIST_SORT,
 
 	MPD_TAG_COUNT
 };