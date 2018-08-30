8d2824ba76bf5558f1479498802b22aa14a652b9
Max Kellermann <max@musicpd.org>
2018-08-30 16:42:50 +0200

    status: move parse_audio_format() to audio_format.c

--- src/status.c
+++ src/status.c
@@ -33,6 +33,7 @@
 #include <mpd/status.h>
 #include <mpd/pair.h>
 #include <mpd/audio_format.h>
+#include "iaf.h"
 
 #include <stdlib.h>
 #include <string.h>
@@ -192,52 +193,6 @@ parse_mpd_state(const char *p)
 		return MPD_STATE_UNKNOWN;
 }
 
-static void
-parse_audio_format(struct mpd_audio_format *audio_format, const char *p)
-{
-	char *endptr;
-
-	if (strncmp(p, "dsd", 3) == 0) {
-		/* allow format specifications such as "dsd64" which
-		   implies the sample rate */
-
-		unsigned long dsd = strtoul(p + 3, &endptr, 10);
-		if (endptr > p + 3 && *endptr == ':' &&
-		    dsd >= 32 && dsd <= 4096 && dsd % 2 == 0) {
-			audio_format->sample_rate = dsd * 44100 / 8;
-			audio_format->bits = MPD_SAMPLE_FORMAT_DSD;
-
-			p = endptr + 1;
-			audio_format->channels = strtoul(p, NULL, 10);
-			return;
-		}
-	}
-
-	audio_format->sample_rate = strtoul(p, &endptr, 10);
-	if (*endptr == ':') {
-		p = endptr + 1;
-
-		if (p[0] == 'f' && p[1] == ':') {
-			audio_format->bits = MPD_SAMPLE_FORMAT_FLOAT;
-			p += 2;
-		} else if (p[0] == 'd' && p[1] == 's' &&
-			   p[2] == 'd' && p[3] == ':') {
-			audio_format->bits = MPD_SAMPLE_FORMAT_DSD;
-			p += 4;
-		} else {
-			audio_format->bits = strtoul(p, &endptr, 10);
-			p = *endptr == ':' ? endptr + 1 : NULL;
-		}
-
-		audio_format->channels = p != NULL
-			? strtoul(p, NULL, 10)
-			: 0;
-	} else {
-		audio_format->bits = 0;
-		audio_format->channels = 0;
-	}
-}
-
 void
 mpd_status_feed(struct mpd_status *status, const struct mpd_pair *pair)
 {
@@ -297,7 +252,7 @@ mpd_status_feed(struct mpd_status *status, const struct mpd_pair *pair)
 	else if (strcmp(pair->name, "updating_db") == 0)
 		status->update_id = atoi(pair->value);
 	else if (strcmp(pair->name, "audio") == 0)
-		parse_audio_format(&status->audio_format, pair->value);
+		mpd_parse_audio_format(&status->audio_format, pair->value);
 }
 
 void mpd_status_free(struct mpd_status * status) {
