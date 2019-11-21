62a474d096f21b175652c0263a501debb713b54f
jcorporation <mail@jcgames.de>
2019-11-21 18:58:40 +0000

    Add new tags "Grouping" (for ID3 "TIT1"), "Work" and "Conductor"

--- include/mpd/tag.h
+++ include/mpd/tag.h
@@ -73,6 +73,9 @@ enum mpd_tag_type
 	MPD_TAG_ALBUM_SORT,
 	MPD_TAG_LABEL,
 	MPD_TAG_MUSICBRAINZ_WORKID,
+	MPD_TAG_GROUPING,
+	MPD_TAG_WORK,
+	MPD_TAG_CONDUCTOR,
 
 	/* IMPORTANT: the ordering of tag types above must be
 	   retained, or else the libmpdclient ABI breaks */
