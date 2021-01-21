c29ed95a4390d0ceb1de3355e7346f053904e79b
jcorporation <mail@jcgames.de>
2021-01-21 23:25:21 +0100

    Add support for new binarylimit command since mpd 0.22.4

--- src/binary.c
+++ src/binary.c
@@ -1,5 +1,5 @@
 /* libmpdclient
-   (c) 2003-2019 The Music Player Daemon Project
+   (c) 2003-2021 The Music Player Daemon Project
    This project's homepage is: http://www.musicpd.org
 
    Redistribution and use in source and binary forms, with or without
@@ -26,61 +26,22 @@
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-/**
- * @mainpage
- *
- * This is a client library for the [Music Player
- * Daemon](https://www.musicpd.org/), written in C.
- *
- * You can choose one of several APIs, depending on your requirements:
- *
- * - struct mpd_async: a very low-level asynchronous API which knows
- *   the protocol syntax, but no specific commands
- *
- * - struct mpd_connection: a basic synchronous API which knows all
- *   MPD commands and parses all responses
- *
- * \author Max Kellermann (max.kellermann@gmail.com)
- */
+#include <mpd/binary.h>
+#include <mpd/send.h>
+#include <mpd/response.h>
+#include "isend.h"
+#include "run.h"
 
-#ifndef MPD_CLIENT_H
-#define MPD_CLIENT_H
+bool
+mpd_send_binarylimit(struct mpd_connection *connection, unsigned limit)
+{
+	return mpd_send_u_command(connection, "binarylimit", limit);
+}
 
-// IWYU pragma: begin_exports
-
-#include "audio_format.h"
-#include "binary.h"
-#include "capabilities.h"
-#include "connection.h"
-#include "database.h"
-#include "directory.h"
-#include "entity.h"
-#include "fingerprint.h"
-#include "idle.h"
-#include "list.h"
-#include "message.h"
-#include "mixer.h"
-#include "mount.h"
-#include "neighbor.h"
-#include "output.h"
-#include "pair.h"
-#include "partition.h"
-#include "password.h"
-#include "player.h"
-#include "playlist.h"
-#include "queue.h"
-#include "recv.h"
-#include "replay_gain.h"
-#include "response.h"
-#include "search.h"
-#include "send.h"
-#include "settings.h"
-#include "song.h"
-#include "stats.h"
-#include "status.h"
-#include "sticker.h"
-#include "version.h"
-
-// IWYU pragma: end_exports
-
-#endif
+bool
+mpd_run_binarylimit(struct mpd_connection *connection, unsigned limit)
+{
+	return mpd_run_check(connection) &&
+		mpd_send_binarylimit(connection, limit) &&
+		mpd_response_finish(connection);
+}
